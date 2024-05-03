203ac8f55d1a081cdc78dedf3b7cb2d3bf54a30c
Matt Way <matt-kiwi@users.noreply.github.com>
2024-05-03 17:20:41 +1200

    added more sutff

--- README.md
+++ README.md
@@ -10,6 +10,10 @@ Other notes;
 Edit sdkconfig with ESP-IDF menuconfig
 pio run -t menuconfig
 
+## Command to read coredump partition to file;
+~/.platformio/packages/tool-esptoolpy/esptool.py -p /dev/cu.usbserial-0001 -b 115200 read_flash 0x3f0000 0x010000 coredump.raw
+
+
 Getting LittleFS to work on IDF
 https://github.com/joltwallet/esp_littlefs
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,51 @@
 #include "utilities.h"
 #include <LittleFS.h>
 #include "FS.h"
+#include "esp_partition.h"
+#include "esp_core_dump.h"
 #define FIRMWARE_VERSION "v@1.0.1"
+#define BUFFER_SIZE 4096
+#define PARTITION_NAME "coredump"
+
+// Borrowed from https://github.com/ddomnik/coredump_example
+
 
 // LittleFS see notes here on using with IDF
 // https://github.com/Jason2866/Arduino_IDF_LittleFS/tree/main
 
 
+
+const esp_partition_t* findCoreDumpPartition()
+{
+    esp_partition_type_t p_type = ESP_PARTITION_TYPE_DATA;
+    esp_partition_subtype_t p_subtype = ESP_PARTITION_SUBTYPE_DATA_COREDUMP;
+    const char *label = PARTITION_NAME;
+
+    return esp_partition_find_first(p_type, p_subtype, label);
+}
+
+void displayCrashInfo(){
+  //
+  const esp_partition_t* partition = findCoreDumpPartition();
+  // esp_core_dump_flash_check();
+}
+
+void displayChipInfo(){
+  esp_chip_info_t chip_info;
+  esp_chip_info(&chip_info);
+  Serial.printf("This chip with %d CPU cores, WiFi%s%s, ",
+            chip_info.cores,
+            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+
+  Serial.printf("silicon revision %d, ", chip_info.revision);
+}
+
 void crash_me_hard() {
 	//provoke crash through writing to a nullpointer
-	volatile uint32_t* aPtr = (uint32_t*) 0x00000000;
-	*aPtr = 0x1234567; //goodnight
+	// volatile uint32_t* aPtr = (uint32_t*) 0x00000000;
+	// *aPtr = 0x1234567; //goodnight
+  assert(0);
 }
 
 void processSerial(){
@@ -32,6 +67,7 @@ void setup() {
   
   printPartitionTable();
   listRootDir();
+  displayChipInfo();
   Serial.printf("\n press C to cause crash\n");
 }
 
